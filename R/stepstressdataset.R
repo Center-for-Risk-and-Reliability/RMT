b698925cea62d058f11582ea07daff84541b2b14
Reuel Smith <51130202+ReuS009@users.noreply.github.com>
2023-12-05 13:54:59 -0500

    v.1.2.6.15

--- R/stepstressdataset.R
+++ R/stepstressdataset.R
@@ -3,6 +3,7 @@
 
 stepstress.data <- function(data,stepstresstable) {
   library(dplyr)
+  library(pracma)
   # Take the time data and the step stress table to set up your data
   # for step-stress analysis
   Ndat<-dim(data)[2]
@@ -15,6 +16,12 @@ stepstress.data <- function(data,stepstresstable) {
   Tv<-integer(0)
   remnantstepstresstable<-integer(0)
   iremnant<-integer(0)
+
+  # Check to see if rownames on stepstresstable exist.  They need to have names in order
+  # for stepstress.LSQest to work properly
+  if(is.null(rownames(stepstresstable))==TRUE){
+    rownames(stepstresstable)<-num2str(c(1:Nss_tbl2),0)
+  }
 
   # Correct or adjust the time if it is not already corrected
   TestTv<-rep(0,length(data[,Ndat]))
@@ -62,6 +69,8 @@ stepstress.data <- function(data,stepstresstable) {
       Slist[[i2]]<-data[,i2+2]
     }
   }
+  # Tally which times are in which step by stress
+  Stepv <- rep(0,Ndat2)
 
   for (i2 in 1:length(stepstresstable[,1])) {
     if(Ndat==3){
@@ -76,6 +85,7 @@ stepstress.data <- function(data,stepstresstable) {
         Tv<-c(Tv,adjT[which(data[,Ndat]==stepstresstable[i2,1])],rep(stepstresstable[i2,2],length(which(data[,Ndat]>stepstresstable[i2,1]))))
         iremnant<-c(iremnant,i2)
       }
+      Stepv[which(data[,Ndat]== stepstresstable[i2,1])] <- i2
     } else if(Ndat>=4){
       for(i3 in 1:Nstress){
         Smatch<-which(data[,i3+2]==stepstresstable[i2,i3])
@@ -100,12 +110,29 @@ stepstress.data <- function(data,stepstresstable) {
         Tv<-c(Tv,adjT[Ssame],rep(stepstresstable[i2,Nss_tbl],Ndat2-last(Ssame)))
         iremnant<-c(iremnant,i2)
       }
+      Stepv[Ssame] <- i2
     }
   }
   if(Ndat==3){
+    stress_v <- logical(0)
+    for(i in 1:Nss_tbl2){
+      if(i==1){
+        stress_v <- rep(stepstresstable[i,1],Ndat2)
+      }else{
+        stress_v <- c(stress_v,rep(stepstresstable[i,1],Ndat2))
+      }
+    }
     stepstressdat <- matrix(c(Tv,Cv,Sv), nrow = length(Tv), ncol = Ndat, byrow = FALSE)
+    updatedata0 <- matrix(c(rep(adjT,Nss_tbl2),rep(data[,2],Nss_tbl2),stress_v), nrow = Nss_tbl2*length(data[,2]), ncol = Ndat, byrow = FALSE)
   } else if (Ndat>=4){
+    stress_v1 <- logical(0)
+    stress_v2 <- logical(0)
+    for(i in 1:Nss_tbl2){
+      stress_v1 <- c(stress_v1,rep(stepstresstable[i,1],Ndat2))
+      stress_v2 <- c(stress_v2,rep(stepstresstable[i,2],Ndat2))
+    }
     stepstressdat <- matrix(c(Tv,Cv,unlist(Sv)), nrow = length(Tv), ncol = Ndat, byrow = FALSE)
+    updatedata0 <- matrix(c(rep(adjT,Nss_tbl2),rep(data[,2],Nss_tbl2),stress_v1,stress_v2), nrow = Nss_tbl2*length(data[,2]), ncol = Ndat, byrow = FALSE)
   }
-  return(list(stepstressdat,stepstresstable[iremnant,]))
+  return(list(stepstressdat,stepstresstable[iremnant,],updatedata0,Stepv))
 }
